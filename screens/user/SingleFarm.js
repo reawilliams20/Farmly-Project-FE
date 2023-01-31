f621fccecb1380125afcee76d6f64dd6bcb4558c
LexieLei98 <Lexie.Lei@kpmg.co.uk>
2023-01-31 10:06:43 +0000

    added singlefarm and myfarm

--- screens/user/SingleFarm.js
+++ screens/user/SingleFarm.js
@@ -1,49 +1,65 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet,Image} from "react-native";
 import { useEffect, useState } from "react";
-import {getProduce} from '../../utils/api'
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {getFarmById, getProduce} from '../../utils/api'
 import { FlatList } from "react-native";
-// import {Avatar, Card, Text} from 'react-native-paper'
+import { Button, Card } from "react-native-elements";
 
-const SingleFarm = ({route}) =>{
+const SingleFarm = ({route, navigation}) =>{
     const {farm_id}= route.params
+    const [farm, setFarm] = useState([]);
     const [produce, setProduce]= useState([]);
+
     useEffect(()=>{
+        getFarmById(farm_id)
+        .then((response) => {
+            setFarm(response)
+        })
         getProduce()
         .then((response)=> {
             setProduce(response)
         });
     },[]); 
-
     const produceInStock= produce.filter((item)=>{
         return item.farm_id === farm_id
     })
-
-     const ProduceCard = ({name, description, produce, unit})=>(
-        <SafeAreaView>
-        
-        <Text>{name}</Text>
-        {/* <TouchableOpacity onPress= {()=>navigation.navigate("SingleFarm", {farm_id: farm_id, description: description, img_url: profile_pic})}
-            <View> 
-        </View>
-        </TouchableOpacity> */}
-        </SafeAreaView>
- )
-
-    return (
-        <View style={styles.container}>
-        <Text>Test</Text>
-        <FlatList
-        data = {produceInStock}
-        renderItem= {(item)=>
-        <ProduceCard 
-        name = {item.name} 
-        description= {item.description} /> }
-        />
-        </View>
-
-    )
+    
+    if(farm.length !== 0) {
+        return (
+            <View style={styles.container}>
+            < Button
+            title="Back"
+            onPress={() => {navigation.navigate("FarmList")}} />
+    
+            <Text>{farm.name}</Text>
+            <Image source={{uri:`${farm.profile_pic}`}}
+            style={{width: 200, height: 100}}/>
+            <Text>{farm.address.street}</Text>
+            <Text>{farm.address.town}</Text>
+            <Text>{farm.address.county}</Text>
+            <Text>{farm.address.postcode}</Text>
+            <Text>{farm.address.country}</Text>
+            <Text>{farm.description}</Text>
+            <Text>{farm.description}</Text>
+    
+            <FlatList
+            data={produceInStock}
+            renderItem={({item}) => {
+                return (
+                    <Card>
+                        <View>
+                            <Text>{item.name}:</Text>
+                            <Text>£{item.price}/per {item.unit}</Text>
+                            <Image source={{uri:`${item.produce_pic}`}}
+                            style={{width:100, height:50}}/>
+                            <Text>{item.description}</Text>
+                        </View>
+                    </Card>
+                )
+            }}/>
+            </View>
+        )
+    }
 }
 
 export default SingleFarm
