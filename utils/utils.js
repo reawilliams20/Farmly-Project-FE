b6d704623bd0f3abf30669ca2eab38a3a7153aa2
joshnguyen98 <josh.nguyen@kpmg.co.uk>
2023-01-31 16:56:08 +0000

    fixed formula

--- utils/utils.js
+++ utils/utils.js
@@ -10,7 +10,7 @@ export function distanceCalculator(currentLat, currentLon, farmPostcode) {
     const lat1 = currentLat
     let lon2 = res.data.result.longitude
     let lat2 = res.data.result.latitude
-    let distance = (12742 * Math.asin(Math.sqrt(0.5 - cos((lat2 - lat1) * pi)/2 + cos(lat1 * pi) * cos(lat2 * pi) * (1 - cos((lon2 - lon1) * pi))/2)))
+    let distance = 12742 * Math.asin(Math.sqrt(0.5 - cos((lat2 - lat1) * pi)/2 + cos(lat1 * pi) * cos(lat2 * pi) * (1 - cos((lon2 - lon1) * pi))/2))
     let roundedDistance = (Math.round(distance * 10) / 10).toFixed(1)
     //Math.round(distance*100)/100
     return roundedDistance
